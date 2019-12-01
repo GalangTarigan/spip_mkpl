bda8fc62d60591ab1bc454560c761c04688a3074
GalangTarigan <53286622+GalangTarigan@users.noreply.github.com>
2019-12-01 22:31:04 +0700

    Penambahan controller statistik teknisi dan proyek

--- app/Http/Controllers/ProyekController.php
+++ app/Http/Controllers/ProyekController.php
@@ -15,7 +15,45 @@ class ProyekController extends Controller
 {
     use Convert;
 
-  
+    public function indexStatsProyek()
+    { //halaman statistik proyek
+        return view('pages.admin.statistikProyek');
+    }
+
+    public function indexDetailProyek(Request $request)
+    { //halaman detail dari proyek
+        $kota = $request->kabupaten_kota;
+        return view('pages.admin.detailProyek', compact('kota'));
+    }
+
+    public function duplicateRow(Request $request)
+    { //table daftar instalasi proyek per provinsi pada halaman statistik proyek  
+        $data = Instansi::distinct('provinsi')->get();
+        $temp = array();
+        $res = array();
+        foreach ($data as $datas) {
+            $temp = Instansi::select('kabupaten_kota')->join('laporan_instalasi', 'laporan_instalasi.instansi_id', '=', 'instansi.id_instansi')
+                ->where('status', '!=', 'Dalam Pengerjaan')
+                ->where('provinsi', $datas->provinsi)->distinct('kabupaten_kota')->get();
+            foreach ($temp as $val) {
+                $flag = true;
+                $var['provinsi'] = $datas->provinsi;
+                $var['projects_amount'] = Instansi::join('laporan_instalasi', 'laporan_instalasi.instansi_id', '=', 'instansi.id_instansi')
+                ->where('kabupaten_kota', $val->kabupaten_kota)->get()->count();
+                $var['kabupaten_kota'] = $val['kabupaten_kota'];
+                foreach ($res as $re) {
+                    if ($re['provinsi'] == $datas->provinsi && $val['kabupaten_kota'] == $re['kabupaten_kota']) {
+                        $flag = false;
+                        break;
+                    }
+                }
+                if ($flag) {
+                    array_push($res, $var);
+                }
+            }
+        }
+        return $res;
+    }
 
     public function showProyek(Request $request)
     { //table daftar instalasi proyek pada halaman proyek instansi
@@ -41,6 +79,67 @@ class ProyekController extends Controller
             array_push($res, $temp);
         }
         return $res;
+    }
+
+    public function detailProyek(Request $request)
+    { // table daftar proyek per kota pada page detail proyek
+
+        $kota = $request->kabupaten_kota;
+        $data = Laporan_Instalasi::with('instansi.daftar_pic')
+            ->join('instansi', 'instansi.id_instansi', '=', 'laporan_instalasi.instansi_id')
+            ->where('instansi.kabupaten_kota', $kota)->get();
+        return $data;
+    }
+
+    public function barProyek(Request $request)
+    { // bar chart statistik instalasi proyek pada page statistik proyek
+        // $from = Carbon::createFromFormat('Y-m-d H:i:s', $this->convertToTimeStamps($request->date_start), 'Asia/Jakarta');
+        // $to = Carbon::createFromFormat('Y-m-d H:i:s', $this->convertToTimeStamps($request->date_end), 'Asia/Jakarta');
+        $data = Laporan_Instalasi::select('nama_instansi', 'instansi_id')->join('instansi', 'instansi.id_instansi', '=', 'laporan_instalasi.instansi_id')
+            ->where('status', '!=', 'Dalam Pengerjaan')->where('waktu_mulai','like',$request->date_start.'%')
+            ->distinct('nama_instansi', 'instansi_id')->get();
+        $title = array();
+        $duration = array();
+        foreach ($data as $project) {
+            $temp_title = $project->nama_instansi;
+            $temp_duration = $this->getDuration($project->instansi_id);
+            array_push($duration, $temp_duration);
+            array_push($title, $temp_title);
+        }
+        return compact('title', 'duration');
+        
+    }
+
+    public function getDuration($instansi_id)
+    {
+        $projects = Laporan_Instalasi::where('instansi_id', $instansi_id)->where('status', '!=', 'Dalam Pengerjaan')->get();
+        $total_time = 0;
+        foreach ($projects as $project) {
+            $total_time = $total_time + (strtotime($project->waktu_mulai) - strtotime($project->waktu_selesai));
+        }
+        $tot = $total_time;
+        $hour = floor($tot / 3600);
+        $tot = $tot % 3600;
+        $min = floor($tot / 60);
+        $tot = $tot % 60;
+        return $hour;
+    }
+
+    public function barDetailProyek(Request $request)
+    { //bar chart statistik instalasi proyek pada halaman statistik detail proyek
+        $data = Laporan_Instalasi::select('nama_instansi', 'instansi_id')->join('instansi', 'instansi.id_instansi', '=', 'laporan_instalasi.instansi_id')
+            ->where('kabupaten_kota', $request->kota)->where('status', '!=', 'Dalam Pengerjaan')
+            ->distinct('nama_instansi', 'instansi_id')->get();
+        $res = array();
+        $title = array();
+        $duration = array();
+        foreach ($data as $project) {
+            $temp_title = $project->nama_instansi;
+            $temp_duration = $this->getDuration($project->instansi_id);
+            array_push($duration, $temp_duration);
+            array_push($title, $temp_title);
+        }
+        return compact('title', 'duration');
     }
 
     //Get list finished projects betweeen two dates
